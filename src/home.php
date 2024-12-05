105a3fe779bba7eec471baa1aefb70ef38dcf8c9
Abhay <abhaylagah01@gmail.com>
2024-12-05 22:05:38 +0000

    Upload product, latest uploads, shop

--- src/home.php
+++ src/home.php
@@ -109,37 +109,100 @@ session_start();
  </section>
 
 
+ <section class="products"> 
+    <!-- Section header -->
+    <!-- <h1 href="shop.php" class="cormorant-garamond-bold">Ultimos Lançamentos</h1>  -->
 
-  <!-- <section class="products"> 
-    <h1 href="shop.php" class="cormorant-garamond-bold">Ultimos Lançamentos</h1> 
-     <div class="box-container">
-        <?php /*
+    <!-- Container for product boxes -->
+    <!-- <div class="box-container"> -->
+        <?php 
+        /*
+          // Query the database to select up to 6 products
           $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+          
+          // Check if there are products in the database
           if(mysqli_num_rows($select_products) > 0){
+           // Loop through each product and display it
            while($fetch_products = mysqli_fetch_assoc($select_products)){
+        */
         ?>
-       <form action="" method="post" class="box">
-          <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-          <div class="name"><?php echo $fetch_products['name']; ?></div>
-          <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-          <input type="number" min="1" name="product_quantity" value="1" class="qty">
-          <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-          <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-          <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-          <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-       </form>
+       <!-- Form for individual product -->
+       <!-- <form action="" method="post" class="box"> -->
+          <!-- Product image -->
+          <!-- <img class="image" src="uploaded_img/<?php // echo $fetch_products['image']; ?>" alt=""> -->
+          <!-- Product name -->
+          <!-- <div class="name"><?php // echo $fetch_products['name']; ?></div> -->
+          <!-- Product price -->
+          <!-- <div class="price">$<?php // echo $fetch_products['price']; ?>/-</div> -->
+          <!-- Quantity input -->
+          <!-- <input type="number" min="1" name="product_quantity" value="1" class="qty"> -->
+          <!-- Hidden inputs for product details -->
+          <!-- <input type="hidden" name="product_name" value="<?php // echo $fetch_products['name']; ?>"> -->
+          <!-- <input type="hidden" name="product_price" value="<?php // echo $fetch_products['price']; ?>"> -->
+          <!-- <input type="hidden" name="product_image" value="<?php // echo $fetch_products['image']; ?>"> -->
+          <!-- Submit button to add product to cart -->
+          <!-- <input type="submit" value="add to cart" name="add_to_cart" class="btn"> -->
+       <!-- </form> -->
        <?php
+       /*
            }
           }else{
+          // Display a message if no products are available
           echo '<p class="empty">Sem produtos no momento!</p>';
-          }*/
+          }
+        */
         ?>
-     </div>
+     <!-- </div> -->
 
-    <div class="load-more" style="margin-top: 2rem; text-align:center">
-       <a href="shop.php" class="option-btn">Veja mais</a>
+    <!-- Link to view more products -->
+    <!-- <div class="load-more" style="margin-top: 2rem; text-align:center"> -->
+       <!-- <a href="shop.php" class="option-btn">Veja mais</a> -->
+    <!-- </div> -->
+ </section>
+
+
+
+ 
+ <section class="carousel">
+    <h1 class="cormorant-garamond-bold">Últimos Lançamentos</h1>
+    <div class="carousel-container">
+        <div class="carousel-items">
+            <?php
+            include 'config.php';
+
+            // Fetch latest 5 books from the database
+            $fetch_products_query = mysqli_query($conn, "SELECT * FROM `products` ORDER BY id DESC LIMIT 5") or die('Query failed');
+
+            if(mysqli_num_rows($fetch_products_query) > 0) {
+                while($product = mysqli_fetch_assoc($fetch_products_query)) {
+                    ?>
+                    <div class="carousel-item">
+                        <img src="uploaded_img/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" class="carousel-image">
+                        <div class="carousel-item-info">
+                            <h3><?php echo $product['name']; ?></h3>
+                            <p class="price">€<?php echo $product['price']; ?></p>
+                            <form action="" method="post">
+                                <input type="hidden" name="product_name" value="<?php echo $product['name']; ?>">
+                                <input type="hidden" name="product_price" value="<?php echo $product['price']; ?>">
+                                <input type="hidden" name="product_image" value="<?php echo $product['image']; ?>">
+                                <!--<input type="number" name="product_quantity" value="1" min="1" class="quantity-box"> -->
+                                <button type="submit" name="add_to_cart" class="add-to-cart-btn">
+                                    <span class="material-icons">local_mall</span> Add to Cart
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    <?php
+                }
+            } else {
+                echo '<p class="empty">Sem produtos disponíveis!</p>';
+            }
+            ?>
+        </div>
     </div>
- </section>-->
+</section>
+
+
 
 
 <section class="carousel">
--- src/shop.php
+++ src/shop.php
@@ -40,9 +40,9 @@
     <p> <a href="home.php">Home</a> / Livros </p>
  </div>
  <section class="products">
-    <h1 class="title">Ultimos Lançamentos</h1>
+    <!--<h1 class="title">Ultimos Lançamentos</h1> -->
     <div class="box-container">
-       <?php  /*
+       <?php  
           $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
           if(mysqli_num_rows($select_products) > 0){
              while($fetch_products = mysqli_fetch_assoc($select_products)){
@@ -50,8 +50,8 @@
        <form action="" method="post" class="box">
           <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
           <div class="name"><?php echo $fetch_products['name']; ?></div>
-          <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-          <input type="number" min="1" name="product_quantity" value="1" class="qty">
+          <div class="price">€<?php echo $fetch_products['price']; ?>/-</div>
+          <!--<input type="number" min="1" name="product_quantity" value="1" class="qty"> -->
           <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
           <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
           <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
@@ -61,7 +61,7 @@
           }
           }else{
              echo '<p class="empty">Sem produtos no momento!</p>';
-            } */
+            } 
         ?>
      </div>
   </section>
