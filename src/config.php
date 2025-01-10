f4cb8369f8074abeaa2d25f2f9377ccbd9276259
Abhay <abhaylagah01@gmail.com>
2025-01-10 15:39:41 +0000

    Trying to resolve conflicts

--- src/config.php
+++ src/config.php
@@ -1,7 +1,11 @@
 <?php
+<<<<<<< HEAD
 $conn = mysqli_connect('db', 'root', 'root', 'shop_db', 3306);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+=======
+$conn = mysqli_connect('localhost', 'root', 'root', 'shop_db');
+>>>>>>> Abhay
 //$conn = mysqli_connect('localhost', 'root', 'root', 'shop_db') or die('connection failed');
 ?>
