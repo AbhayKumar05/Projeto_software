c61f93c250ae32935ef94a6c2547cd5e2378e82f
Abhay <abhaylagah01@gmail.com>
2025-01-10 13:45:54 +0000

    Changes in Dockerfile.yml

--- src/config.php
+++ src/config.php
@@ -1,3 +1,7 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'root', 'shop_db') or die('connection failed');
+$conn = mysqli_connect('db', 'root', 'root', 'shop_db', 3306);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//$conn = mysqli_connect('localhost', 'root', 'root', 'shop_db') or die('connection failed');
 ?>
